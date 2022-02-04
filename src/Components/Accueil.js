fd97a54ac7604bcf1f59ca5e41eea9893fe5347e
Wilt Lilian <70480338+LilianWilt120100@users.noreply.github.com>
2022-02-04 14:36:36 +0100

    init depot + debut projet

---
+++ src/Components/Accueil.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Navbar } from './Navbar';
+
+export class Accueil extends Component{
+    render(){
+    return(
+        <>
+            <Navbar />
+            <hr />
+        </>
+    )
+    }
+}
+
