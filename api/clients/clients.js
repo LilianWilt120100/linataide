b2e6709bc4b0a4326bd0d562c624380272105903
Wilt Lilian <70480338+LilianWilt120100@users.noreply.github.com>
2022-02-10 13:21:24 +0100

    possible voir clients + supprimer clients

--- api/clients/clients.js
+++ api/clients/clients.js
@@ -40,5 +40,49 @@ module.exports = function (app, db) {
           }
         })
     });
+
+    app.get(`/clientid/:nom`, (req, res) => {
+      let nom = req.params.nom;
+      db.query(`SELECT idClient FROM clients WHERE nom=?`, [
+        nom,
+      ])
+        .then((result) => {
+          res.send(result)
+        })
+        .catch((err) => {
+          if (err) {
+            console.log(err);
+            res.status(404).send({
+              statusCode: 404,
+              id: 1,
+              message: err.data,
+              content: err
+            })
+          }
+        })
+    });
+
+
+    app.delete(`/deleteclient/:id`, (req, res) => {
+      let id = req.params.id;
+      db.query(`DELETE FROM clients WHERE idClient=?`, [
+        id,
+      ])
+        .then((result) => {
+          //res.send(result)
+          res.send("Client avec id "+id+" supprimé")
+        })
+        .catch((err) => {
+          if (err) {
+            console.log(err);
+            res.status(404).send({
+              statusCode: 404,
+              id: 1,
+              message: err.data,
+              content: err
+            })
+          }
+        })
+    });
     
   }
--- front/src/Components/VueClients.js
+++ front/src/Components/VueClients.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom';
 import { Navbar } from './Navbar';
 import MenuVertical from './MenuVertical';
 import axios from 'axios';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Button from './Button';
 
 export class VueClient extends Component{
     
     state={
-        tabClients:[]
+        tabClients:[],
     }
 
     componentDidMount() { //Recupere les données 
@@ -20,7 +28,20 @@ export class VueClient extends Component{
             console.log(err)
           });
     
-      }
+    }
+
+
+    suppClient(id){
+      axios.delete(`http://localhost:3001/deleteclient/`+id)
+          .then((result) => {
+            alert("Client "+ id + " supprimé")
+          }).catch((err) => {
+            console.log(err)
+          });
+
+          window.location.reload(false);
+    
+    }
 
     render(){
         
@@ -28,9 +49,41 @@ export class VueClient extends Component{
         <>  
         
             <div>
-                
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 700 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell style={{color: "blue", fontSize:"14px"}}>ID</TableCell>
+                    <TableCell align="right" style={{color: "blue", fontSize:"14px"}}>Nom</TableCell>
+                    <TableCell align="right" style={{color: "blue", fontSize:"14px"}}>Prenom</TableCell>
+                    <TableCell align="right" style={{color: "blue", fontSize:"14px"}}>Email</TableCell>
+                    <TableCell align="right" style={{color: "blue", fontSize:"14px"}}>Ville</TableCell>
+                    <TableCell align="right" style={{color: "blue", fontSize:"14px"}}>Adresse</TableCell>
+                    <TableCell align="right" style={{color: "blue", fontSize:"14px"}}>Supprimer</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {this.state.tabClients.map((client) => (
+                  <TableRow key={client.idClient} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                    <TableCell component="th" scope="row">{client.idClient}</TableCell>
+                    <TableCell align="right">{client.nom}</TableCell>
+                    <TableCell align="right">{client.prenom}</TableCell>
+                    <TableCell align="right">{client.email}</TableCell>
+                    <TableCell align="right">{client.ville}</TableCell>
+                    <TableCell align="right">{client.adresse}</TableCell>
+                    <TableCell align="right"><Button onClick={(e) => this.suppClient(client.idClient)} width="120px" height="35px" color="red" fSize="16px" float="right">Supprimer</Button>  </TableCell>
+
+                    </TableRow>
+
+                  ))}
+                  
+
+
+                </TableBody>
+              </Table>
+            </TableContainer>
             </div>
-            
+                
         </>
     )
     }
